2c3acb0ed1b5a1f609f6694b2a6bdf12e07af998
Liam Labuschagne <=>
2025-02-04 12:57:40 +1300

    update login to set name in firestore

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -6,6 +6,8 @@
 <script setup lang="ts">
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { useRouter } from 'vue-router'
+import { doc, setDoc } from 'firebase/firestore'
+import { db } from '../firebase'
 const router = useRouter()
 const provider = new GoogleAuthProvider()
 const auth = getAuth()
@@ -13,7 +15,19 @@ const auth = getAuth()
 const signinWithGoogle = () => {
   signInWithPopup(auth, provider)
     .then((result) => {
-      router.push('/')
+      setDoc(
+        doc(db, 'foodspends', auth.currentUser.uid),
+        {
+          name: result.user.displayName,
+        },
+        { merge: true },
+      )
+        .then(() => {
+          router.push('/')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     })
     .catch((error) => {
       console.log(error)
